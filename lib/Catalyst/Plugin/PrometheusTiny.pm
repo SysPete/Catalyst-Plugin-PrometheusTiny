69666f8a03fce341c4358cebf42e7122ca67092e
Peter Mottram <peter@sysnix.com>
2021-06-05 16:21:46 +0200

    add documentation

--- lib/Catalyst/Plugin/PrometheusTiny.pm
+++ lib/Catalyst/Plugin/PrometheusTiny.pm
@@ -3,6 +3,8 @@ package Catalyst::Plugin::PrometheusTiny;
 use strict;
 use warnings;
 
+our $VERSION = '0.001';
+
 use Carp            ();
 use Catalyst::Utils ();
 use Moose::Role;
@@ -130,12 +132,149 @@ sub index : Path Args(0) {
     my $res = $c->res;
     $res->content_type("text/plain");
     $res->output( $c->prometheus->format );
-    return;
 }
 
 =head1 NAME
 
 Catalyst::Plugin::PrometheusTiny - use Prometheus::Tiny with Catalyst
+
+=head1 SYNOPSIS
+
+Use the plugin in your application class:
+
+    package MyApp;
+    use Catalyst 'PrometheusTiny';
+
+    MyApp->setup;
+
+Add more metrics:
+
+    MyApp->config('Plugin::PrometheusTiny' => {
+        metrics => [
+            {
+                name    => 'myapp_thing_to_measure',
+                help    => 'Some thing we want to measure',
+                type    => 'histogram',
+                buckets => [ 1, 50, 100, 1_000, 50_000, 500_000, 1_000_000 ],
+            },
+            {
+                name    => 'myapp_something_else_to_measure',
+                help    => 'Some other thing we want to measure',
+                type    => 'counter',
+            },
+        ],
+    });
+
+And somewhere in your controller classes:
+
+    $c->prometheus->observe_histogram(
+        'myapp_thing_to_measure', $value, { label1 => 'foo' }
+    );
+
+    $c->prometheus->inc(
+        'myapp_something_else_to_measure', $value, { label2 => 'bar' }
+    );
+
+Once your app has served from requests you can fetch request/response metrics:
+
+    curl http://$myappaddress/metrics
+
+=head1 DESCRIPTION
+
+This plugin integrates L<Prometheus::Tiny::Shared> with your L<Catalyst> app,
+providing some default metrics for requests and responses, with the ability
+to easily add further metrics to your app. A default controller is included
+which makes the metrics available via the C</metrics> endpoint, though this
+can be disabled if you prefer to add your own controller action.
+
+See L<Prometheus::Tiny> for more details of the kind of metrics supported.
+
+The following metrics are included by default:
+
+    {
+        name    => 'http_request_duration_seconds',
+        help    => 'Request durations in seconds',
+        type    => 'histogram',
+    },
+    {
+        name    => 'http_request_size_bytes',
+        help    => 'Request sizes in bytes',
+        type    => 'histogram',
+        buckets => [ 1, 50, 100, 1_000, 50_000, 500_000, 1_000_000 ],
+    },
+    {
+        name    => 'http_requests_total',
+        help    => 'Total number of http requests processed',
+        type    => 'counter',
+    },
+    {
+        name    => 'http_response_size_bytes',
+        help    => 'Response sizes in bytes',
+        type    => 'histogram',
+        buckets => [ 1, 50, 100, 1_000, 50_000, 500_000, 1_000_000 ],
+    }
+
+=head1 METHODS
+
+=head2 prometheus
+
+    sub my_action {
+        my ( $self, $c ) = @_;
+
+        $c->prometheus->inc(...);
+    }
+
+Returns the C<Prometheus::Tiny::Shared> instance.
+
+=head1 CONFIGURATION
+
+=head2 filename
+
+It is recommended that this is set to a directory on a memory-backed
+filesystem. See L<Prometheus::Tiny::Shared/filename> for details and default
+value.
+
+=head2 ignore_path_regex
+
+    ignore_path_regex => '^(healthcheck|foobar)'
+
+A regular expression against which C<< $c->request->path >> is checked, and
+if there is a match then the request is not added to default request/response
+metrics.
+
+=head2 metrics
+
+    metrics => [
+        {
+            name => $metric_name,
+            help => $metric_help_text,
+            type => $metric_type,
+        },
+        # more...
+    ]
+
+See L<Prometheus::Tiny/declare>. Declare extra metrics to be added to those
+included with the plugin.
+
+=head2 no_default_controller
+
+    no_default_controller => 0      # default
+
+If set to a true value then the default C</metrics> endpoint will not be
+added, and you will need to add your own controller action for exporting the
+metrics. Something like:
+
+    package MyApp::Controller::Stats;
+
+    sub begin : Private { }
+    sub end  : Private  { }
+
+    sub index : Path Args(0) {
+        my ( $self, $c ) = @_;
+        my $res = $c->res;
+        $res->content_type("text/plain");
+        $res->output( $c->prometheus->format );
+    }
 
 =cut
 
